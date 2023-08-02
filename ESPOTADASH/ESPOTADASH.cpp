ccd053bdc52f9d5872491c0665c5097755379d2d
ErfanDL <14868771+ErfanDL@users.noreply.github.com>
2023-08-02 22:33:50 +0330

    Fixed bugs

---
+++ ESPOTADASH/ESPOTADASH.cpp
@@ -0,0 +1,171 @@
+#include "ESPOTADASH.h"
+
+ESPOTADASH::ESPOTADASH(const char* ssid, const char* password, const char* hostName, const char* serverAddress, unsigned long heartbeatInterval, unsigned long registrationInterval, const char* firmwareVersion) {
+  this->ssid = ssid;
+  this->password = password;
+  this->hostName = hostName;
+  this->serverAddress = serverAddress;
+  this->heartbeatInterval = heartbeatInterval;
+  this->registrationInterval = registrationInterval;
+  this->firmwareVersion = firmwareVersion;
+}
+
+void ESPOTADASH::begin() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting to WiFi...");
+    delay(1000);
+  }
+  // Register with the Node.js server on startup
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFi.hostname(hostName);
+    Serial.print("Registering host name: ");
+    Serial.println(hostName);
+    registerToNodeJS(hostName, firmwareVersion, WiFi.macAddress().c_str(), WiFi.RSSI());
+    lastRegistrationTime = millis(); // Set the last registration time to the current time
+  }
+}
+
+// Helper function to manually encode the hostName
+String ESPOTADASH::urlEncode(const String& str) {
+  String encodedString = "";
+  char c;
+  char bufHex[3];
+  for (unsigned int i = 0; i < str.length(); i++) {
+    c = str.charAt(i);
+    if (isAlphaNumeric(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+      encodedString += c;
+    } else if (c == ' ') {
+      encodedString += "%20";
+    } else {
+      sprintf(bufHex, "%%%02X", c);
+      encodedString += bufHex;
+    }
+  }
+  return encodedString;
+}
+
+void ESPOTADASH::loop() {
+  // Check if firmware flashing is ongoing
+  if (isFirmwareFlashing) {
+    // The firmware flashing process is handled by ESP8266httpUpdate or ESP32httpUpdate automatically.
+    // No additional action is required here.
+  } else {
+    // If not flashing, check if a firmware update is available and start the update process
+    if (WiFi.status() == WL_CONNECTED) {
+      unsigned long currentMillis = millis();
+      // Check if it's time to send a heartbeat
+      if (currentMillis - lastHeartbeatTime >= heartbeatInterval) {
+        sendHeartbeat();
+        lastHeartbeatTime = currentMillis; // Update the last heartbeat time
+      }
+      // Check if it's time to re-register with the server
+      if (currentMillis - lastRegistrationTime >= registrationInterval) {
+        reRegisterDevice();
+        lastRegistrationTime = currentMillis; // Update the last registration time
+      }
+      WiFiClient client;
+      HTTPClient http;
+      String updateURL = (String(serverAddress) + "/updateStatus?hostName=" + urlEncode(hostName)); // Use the urlEncode() function here
+      http.begin(client, updateURL);
+      int httpCode = http.GET();
+      if (httpCode == HTTP_CODE_OK) {
+        String response = http.getString();
+        if (response == "Update Available") {
+          Serial.println("Firmware update available. Starting OTA update...");
+          sendFirmwareFlashingInitiated();
+          isFirmwareFlashing = true; // Set the flag to indicate that firmware flashing is ongoing
+          #if defined(ESP8266)
+		  ESPhttpUpdate.setLedPin(2, LOW);
+          t_httpUpdate_return ret = ESPhttpUpdate.update(client, String(serverAddress) + "/upload/" + urlEncode(hostName) + "_firmware.bin"); // Use urlEncode() function here
+          #elif defined(ESP32)
+          t_httpUpdate_return ret = ESP32shttpUpdate.update(String(serverAddress) + "/upload/" + urlEncode(hostName) + "_firmware.bin", firmwareVersion); // Use urlEncode() function here
+          #endif
+          switch (ret) {
+            case HTTP_UPDATE_FAILED:
+              break;
+            case HTTP_UPDATE_NO_UPDATES:
+              break;
+            case HTTP_UPDATE_OK:
+              Serial.println("OTA Update Completed!");
+              ESP.restart();
+              break;
+          }
+        } else {
+        }
+      } else {
+      }
+      http.end();
+    }
+  }
+}
+
+void ESPOTADASH::registerToNodeJS(const char* hostName, const char* firmwareVersion, const char* macAddress, int wifiSignalStrength) {
+  // Connect to the Node.js server using HTTP POST request
+  WiFiClient client;
+  HTTPClient http;
+  // Construct the registration URL with the host name and firmware version parameters
+  String registrationURL = String(serverAddress) + "/register";
+  http.begin(client, registrationURL);
+  // Set the host name, firmware version, MAC address, Wi-Fi signal strength, and IP address as the request data
+  String postData = String(hostName) + "\n" + firmwareVersion + "\n" + macAddress + "\n" + wifiSignalStrength + "\n" + WiFi.localIP().toString();
+  http.addHeader("Content-Type", "text/plain");
+  int httpCode = http.POST(postData);
+  if (httpCode > 0) {
+    Serial.printf("Registered with Node.js server. HTTP code: %d\n", httpCode);
+  } else {
+    Serial.printf("Failed to connect to Node.js server. HTTP error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+}
+
+void ESPOTADASH::sendHeartbeat() {
+  // Connect to the Node.js server using HTTP POST request
+  WiFiClient client;
+  HTTPClient http;
+  // Construct the heartbeat URL with the host name parameter
+  String heartbeatURL = String(serverAddress) + "/heartbeat";
+  http.begin(client, heartbeatURL);
+  // Set the host name as the request data
+  String postData = String(hostName);
+  http.addHeader("Content-Type", "text/plain");
+  int httpCode = http.POST(postData);
+  if (httpCode > 0) {
+    Serial.printf("Heartbeat sent to Node.js server. HTTP code: %d\n", httpCode);
+  } else {
+    Serial.printf("Failed to send heartbeat to Node.js server. HTTP error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
+void ESPOTADASH::reRegisterDevice() {
+  // Re-register with the Node.js server on a periodic interval
+  registerToNodeJS(hostName, firmwareVersion, WiFi.macAddress().c_str(), WiFi.RSSI());
+}
+
+void ESPOTADASH::sendFirmwareFlashingInitiated() {
+    // Connect to the Node.js server using HTTP POST request
+    WiFiClient client;
+    HTTPClient http;
+
+    // Construct the URL for sending the firmware flashing initiated message
+    String firmwareInitiatedURL = String(serverAddress) + "/firmwareInitiated?hostName=" + urlEncode(hostName); // Include the hostname as a query parameter
+
+    http.begin(client, firmwareInitiatedURL);
+
+    // Set the request data (you can include additional data if needed)
+    String postData = "Hello"; // Modify this line to send the "Hello" message or any other data
+    http.addHeader("Content-Type", "text/plain");
+    int httpCode = http.POST(postData);
+
+    if (httpCode > 0) {
+        Serial.printf("Firmware flashing initiated message sent to Node.js server. HTTP code: %d\n", httpCode);
+    } else {
+        Serial.printf("Failed to send firmware flashing initiated message to Node.js server. HTTP error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+}
+
